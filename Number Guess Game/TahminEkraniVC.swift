6ec695adea68aecae29adb67c0e90ca0512b6d79
omer <gunekfaruk@hotmail.com>
2022-10-20 13:17:51 +0300

    TahminEkraniVC

---
+++ Number Guess Game/TahminEkraniVC.swift
@@ -0,0 +1,18 @@
+//
+//  TahminEkraniVC.swift
+//  Number Guess Game
+//
+//  Created by Ömer Günek on 20.10.2022.
+//
+
+import UIKit
+
+class TahminEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
