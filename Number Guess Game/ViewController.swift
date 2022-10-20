cdc64deae73c3879f7dcb74975b68ed24d75027b
omer <gunekfaruk@hotmail.com>
2022-10-20 13:11:57 +0300

    Initial Commit

---
+++ Number Guess Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Number Guess Game
+//
+//  Created by Ömer Günek on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
