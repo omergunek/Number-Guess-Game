d29eb16ab7ee40c1f434b94906fc7065e2eb823b
omer <gunekfaruk@hotmail.com>
2022-10-20 13:18:06 +0300

    SonucEkraniVC

---
+++ Number Guess Game/SonucEkraniVC.swift
@@ -0,0 +1,18 @@
+//
+//  SonucEkraniVC.swift
+//  Number Guess Game
+//
+//  Created by Ömer Günek on 20.10.2022.
+//
+
+import UIKit
+
+class SonucEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
